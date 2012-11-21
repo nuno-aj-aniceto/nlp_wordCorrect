4608e8fe52aca0d145c7c76f578b1e3150d7f071
Ana <annara.snow@gmail.com>
2012-11-21 11:06:40 +0000

    Jaccard

---
+++ src/ist/ln/mp2/Jaccard.java
@@ -0,0 +1,75 @@
+package ist.ln.mp2;
+
+import java.util.ArrayList;
+
+public class Jaccard {
+
+	private String _str1 = "";
+	private String _str2 = "";
+
+	public Jaccard(String str1, String str2){
+	
+		_str1 = str1;
+		_str2 = str2; 
+	}
+	
+	public int union(){
+		ArrayList<String> union = new ArrayList<String>();
+		int str1_len = _str1.length();
+		int str2_len = _str2.length();
+		
+		for(int i=0; i < str1_len; i++){
+			
+			String s = _str1.charAt(i)+"";
+			
+			if(!union.contains(s)){
+				union.add(s);
+			}
+		}
+		
+		for(int i=0; i < str2_len; i++){
+			
+			String s = _str2.charAt(i)+"";
+			
+			if(!union.contains(s)){
+				union.add(s);
+			}
+		}
+		
+		return union.size();
+	}
+	
+	public int intersection(){
+		ArrayList<String> union = new ArrayList<String>();
+		int str1_len = _str1.length();
+		
+		for(int i=0; i < str1_len; i++){
+			
+			String s = _str1.charAt(i)+"";
+			
+			if(_str2.contains(s)){
+				if(!union.contains(s)){
+					union.add(s);
+				}
+			}
+		}
+	
+		return union.size();
+	}
+	
+	
+	public double checkJaccard(){
+		
+		double i = intersection();
+		double j = union();
+		return i/j;
+		
+	}
+	
+	/**public static void main(String[] args){
+		
+		Jaccard j = new Jaccard("saturday", "sunday");
+		
+		System.out.println(j.checkJaccard());
+	}*/
+}
