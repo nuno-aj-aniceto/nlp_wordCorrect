ce959c0c3e32a588852c1b459006df67f7a297aa
Ana <annara.snow@gmail.com>
2012-11-21 16:16:26 +0000

    testing jaccard

---
+++ src/ist/ln/mp2/NormalizerSimple.java
@@ -0,0 +1,44 @@
+package l2f.nlp;
+
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+
+public class NormalizerSimple {
+
+	/**
+	 * contains:
+	 * 	-remove all '!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'  = \p{Punct}+
+	 *  -lowercase
+	 *  -trim
+	 *  -remove ¿ characters (Punct doesn't capture it)
+	 *  
+	 * @param words: single word or sentence
+	 * @return single word or sentence normalized
+	 */
+	public static String normPunctLCase(String words){
+		return words.replaceAll("\\p{Punct}+", "").replaceAll("¿", "").toLowerCase().trim();
+	}
+	
+	/**
+	 * contains:
+	 * 	-remove all !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~  = \p{Punct}+
+	 *  -lowercase
+	 *  -trim
+	 *  -remove all diacritical marks (´`~^, etc)
+	 * 
+	 * @param words
+	 * @return
+	 */
+	public static String normPunctLCaseDMarks(String words){
+		return Normalizer.normalize(NormalizerSimple.normPunctLCase(words), Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+	}
+	
+	/**
+	 * remove all diacritical marks (´`~^, etc)
+	 * @param words
+	 * @return
+	 */
+	public static String normDMarks(String words){
+		return Normalizer.normalize(words, Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+	}
+}
